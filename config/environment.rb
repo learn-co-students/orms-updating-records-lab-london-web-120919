acd3ae9598c44494ae4b11c769735ef29b983f8b
rmiri <re_miriuk@hotmail.com>
2019-12-18 16:55:44 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,7 @@ require 'sqlite3'
 require_relative '../lib/student'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
+
+
+  # Remember, you can access your database connection anywhere in this class
+  #  with DB[:conn]
